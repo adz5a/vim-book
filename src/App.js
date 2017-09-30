e0f9c41ec2d9dca44485616e3fa8f4a9c6fd2e20
adz5a <antoine.d.zimmermann@gmail.com>
2017-09-30 18:46:17 +0200

    add tachyons css and show it works

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import {
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App helvetica">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro b">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Message1 
