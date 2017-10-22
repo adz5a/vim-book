48481b90af9f10b68ca22962b98a5713bf076cc0
antoine zimmermann <antoine.zimmermann@webdia-group.com>
2017-10-22 23:53:16 +0200

    fix startup

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,62 +1,68 @@
+import xs, { Stream } from "xstream";
+
 
 interface Options {
-    repo: string;
-    author: string;
-    path?: string;
+  repo: string;
+  author: string;
+  path?: string;
 }
 
 /*
  * https://developer.github.com/v3/repos/contents/
  */
 interface ApiResponse {
-    type: "file"| "dir";
-    encoding: "base64" | null;
-    size: number;
-    name: string;
-    path: string;
-    content: string;
-    sha: string;
-    url: string;
-    git_url: string;
-    html_url: string;
-    download_url: string;
-    _links: {
-        git: string;
-        self: string;
-        html: string;
-    }
+  type: "file"| "dir";
+  encoding: "base64" | null;
+  size: number;
+  name: string;
+  path: string;
+  content: string;
+  sha: string;
+  url: string;
+  git_url: string;
+  html_url: string;
+  download_url: string;
+  _links: {
+    git: string;
+    self: string;
+    html: string;
+  }
 }
 
 interface ApiPartialResponse {
-    type: "file" | "dir";
-    size: number;
-    name: string;
-    path: string;
-    sha: string;
-    url: string;
-    git_url: string;
-    html_url: string;
-    download_url: string | null;
-    _links: {
-        self: string;
-        git: string;
-        html: string;
-    }
+  type: "file" | "dir";
+  size: number;
+  name: string;
+  path: string;
+  sha: string;
+  url: string;
+  git_url: string;
+  html_url: string;
+  download_url: string | null;
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  }
 }
 
 const baseURL = "https://api.github.com";
 
 export const makeURL = ( options: Options ) => {
-    if (options.path !== undefined) {
-        return `${baseURL}/repos/${options.author}/${options.repo}/contents/${options.path}`;
-    } else {
-        return `${baseURL}/repos/${options.author}/${options.repo}`;
-    }
+  if (options.path !== undefined) {
+    return `${baseURL}/repos/${options.author}/${options.repo}/contents/${options.path}`;
+  } else {
+    return `${baseURL}/repos/${options.author}/${options.repo}`;
+  }
 };
 
 export const getContents = ( options: Options ): Promise<ApiResponse | ApiPartialResponse[]> => {
-    return fetch(makeURL(options))
-        .then( response => {
-            return response.json()
-        }) as Promise<ApiResponse>;
+  return fetch(makeURL(options))
+    .then( response => {
+      return response.json()
+    }) as Promise<ApiResponse>;
+};
+
+export const getContents$ = ( options: Options ): Stream<ApiResponse | ApiPartialResponse[]> => {
+  return xs.fromPromise(getContents(options));
 };
--- src/data/middleware.js
+++ src/data/middleware.js
@@ -1,5 +1,5 @@
 import { createMiddleware } from "xstream-redux-observable";
-import { getContents } from "./api";
+import { getContents$ } from "./api";
 import { ACTIONS, withType, asType, makeReducer } from "./actions";
 import xs from "xstream";
 import delay from "xstream/extra/delay";
@@ -10,151 +10,161 @@ const PouchDB = global.PouchDB;
 const db = new PouchDB("book");
 global.bookDB = db;
 
-const fetchTOC = () => xs.fromPromise(getContents({
-    author,
-    repo,
-    path: "chapters"
-}));
+const fetchTOC = () => getContents$({
+  author,
+  repo,
+  path: "chapters"
+});
 
 const updateCachedChapter = chapter => db.get(chapter.path)
-    .then(doc => db.put({
+  .then(doc => db.put({
+    _id: chapter.path,
+    _rev: doc._rev,
+    chapter
+  }), error => {
+    if ( error.status === 404 ) {
+      return db.put({
         _id: chapter.path,
-        _rev: doc._rev,
         chapter
-    }), error => {
-        if ( error.status === 404 ) {
-            return db.put({
-                _id: chapter.path,
-                chapter
-            });
-        } else throw error;
-    });
+      });
+    } else throw error;
+  });
 
 export const middleware = createMiddleware( action$ => {
 
-    const loadTOC$ = action$.filter(withType(ACTIONS.loadTOC))
-        .map(() => xs.fromPromise(db.get("toc")
-            .then(doc => doc.toc)))
-        .flatten()
-        .map(asType(ACTIONS.tocLoaded))
-        .replaceError(e => {
-            return xs.of({
-                type: "toc-cache-error",
-                data: e
-            }); 
-        });
+  const loadTOC$ = action$.filter(withType(ACTIONS.loadTOC))
+    .map(() => xs.fromPromise(db.get("toc")
+      .then(doc => doc.toc)))
+    .flatten()
+    .map(asType(ACTIONS.tocLoaded))
+    .replaceError(e => {
+      return xs.of({
+        type: ACTIONS.tocCacheError,
+        data: e
+      }); 
+    });
 
-    const fetchMissingTOC$ = action$
-        .filter(action => {
-            const { type, data: error } = action;
-            return type === ACTIONS.tocCacheError && error.status === 404;
-        })
-        .map(fetchTOC)
-        .flatten()
-        .map(asType(ACTIONS.fetchedTOC));
+  const fetchMissingTOC$ = action$
+    .filter(action => {
+      const { type, data: error } = action;
+      return type === ACTIONS.tocCacheError && error.status === 404;
+    })
+    .map(fetchTOC)
+    .flatten()
+    .map(asType(ACTIONS.fetchedTOC));
 
-    const saveFetchedTOC$ = action$.filter(withType(ACTIONS.fetchedTOC))
-        .map(({ data: toc }) => xs.fromPromise(db.put({
-            _id: "toc",
-            toc
-        })))
-        .flatten()
-        .map(toc => xs.of({
-            type: ACTIONS.tocLoaded,
-            data: toc
-        }, {
-            type: ACTIONS.savedTOC,
-            data: toc
+  const saveFetchedTOC$ = action$.filter(withType(ACTIONS.fetchedTOC))
+    .map(({ data: toc }) => xs.fromPromise(db.put({
+      _id: "toc",
+      toc
+    })))
+    .flatten()
+    .map(toc => xs.of({
+      type: ACTIONS.tocLoaded,
+      data: toc
+    }, {
+      type: ACTIONS.savedTOC,
+      data: toc
+    }))
+    .flatten();
+
+  const fetchAllChapter$ = action$.filter(withType(ACTIONS.missingChaptersContent))
+    .map(({ type, data: chapters }) => {
+      /*
+       * Array<Stream<Chapter>>
+       */
+      const chapters$Array = chapters.map((chapter, index) => xs
+        .of(chapter)
+        .compose(delay(1000 * index))
+        .map(chapter => getContents$({
+          author,
+          repo,
+          path: chapter.path
+        })));
+
+      return xs.merge.apply(null, chapters$Array).flatten();
+    })
+    .flatten()
+    .map(asType(ACTIONS.chapterContentLoaded));
+
+  // hwen tocs are loaded
+  // look up in the db db for correspdongi chatpers
+  const loadAllChapterFormCache$ = action$
+    .filter(withType(ACTIONS.tocLoaded))
+    .map(({ data: chapters }) => {
+
+      /*
+       * TODO: add test for this tiny tiny logic
+       */
+      return xs
+        .fromPromise(db.bulkGet({
+          docs: chapters.map(chapter => ({ id: chapter.path }))
         }))
-        .flatten();
+        .map(response => {
+          console.log(response);
 
-    const fetchAllChapter$ = action$.filter(withType(ACTIONS.missingChaptersContent))
-        .map(({ type, data: chapters }) => {
-            /*
-              Array<Stream<Chapter>>
-            */
-            const chapters$Array = chapters.map((chapter, index) => xs
-                .of(chapter)
-                .compose(delay(1000 * index))
-                .map(chapter => xs.fromPromise(getContents({
-                    author,
-                    repo,
-                    path: chapter.path
-                }))));
+          const {
+            missing,
+            found
+          } = response.results
+            .reduce((results, item) => {
+              const OKs = item.docs
+                .filter(({ ok = {} }) => {
+                  return ok.chapter;
+                } );
 
-            return xs.merge.apply(null, chapters$Array).flatten();
-        })
-        .flatten()
-        .map(asType(ACTIONS.chapterContentLoaded));
+              if ( OKs.length > 0 ) {
+                results.found.push(OKs[0].ok.chapter);
+              } else {
+                results.missing.push({
+                  path: item.id
+                });
+              }
+              return results;
+            }, {
+              missing: [],
+              found: []
+            });
 
-    // hwen tocs are loaded
-    // look up in the db db for correspdongi chatpers
-    const loadAllChapterFormCache$ = action$
-        .filter(withType(ACTIONS.tocLoaded))
-        .map(({ data: chapters }) => {
-
-            return xs
-                .fromPromise(db.bulkGet({
-                    docs: chapters.map(chapter => ({ id: chapter.path }))
-                }))
-                .map(response => {
-                    console.log(response);
-                    const {
-                        missing,
-                        found
-                    } = response.results.reduce((results, item) => {
-                        if ( item.docs.length > 0 ) {
-                            results.found.push(item.docs[0].ok.chapter);
-                        } else {
-                            results.missing.push({
-                                path: item.id
-                            });
-                        }
-                        return results;
-                    }, {
-                        missing: [],
-                        found: []
-                    });
-
-                    return xs.of(
-                        { 
-                            type: ACTIONS.loadedCachedChaptersContent,
-                            data: found
-                        },
-                        {
-                            type: ACTIONS.missingChaptersContent,
-                            data: missing
-                        }
-                    );
-                })
-                .flatten();
-
+          return xs.of(
+            { 
+              type: ACTIONS.loadedCachedChaptersContent,
+              data: found
+            },
+            {
+              type: ACTIONS.missingChaptersContent,
+              data: missing
+            }
+          );
         })
         .flatten();
 
-    // each time a chapter is loaded, save it in the local db
-    const saveChapterContent$ = action$
-        .filter(withType(ACTIONS.chapterContentLoaded))
-        .map(({ data: chapter }) => xs
-            .fromPromise(updateCachedChapter(chapter))
-            .map(() => ({
-                type: ACTIONS.chapterContentSaved,
-                data: chapter
-            })))
-        .flatten();
+    })
+    .flatten();
+
+  // each time a chapter is loaded, save it in the local db
+  const saveChapterContent$ = action$
+    .filter(withType(ACTIONS.chapterContentLoaded))
+    .map(({ data: chapter }) => xs
+      .fromPromise(updateCachedChapter(chapter))
+      .map(() => ({
+        type: ACTIONS.chapterContentSaved,
+        data: chapter
+      })))
+    .flatten();
 
 
-    return xs.merge(
-        loadTOC$,
-        fetchMissingTOC$,
-        saveFetchedTOC$,
-        loadAllChapterFormCache$,
-        fetchAllChapter$,
-        saveChapterContent$,
-        xs.of({ type: ACTIONS.loadTOC })
-    )
-        .replaceError(e => {
-            console.error(e);
-            return xs.empty();
-        });
+  return xs.merge(
+    loadTOC$,
+    fetchMissingTOC$,
+    saveFetchedTOC$,
+    loadAllChapterFormCache$,
+    fetchAllChapter$,
+    saveChapterContent$,
+    xs.of({ type: ACTIONS.loadTOC })
+  )
+    .replaceError(e => {
+      console.error(e);
+      return xs.empty();
+    });
 });
