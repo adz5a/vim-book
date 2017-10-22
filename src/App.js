fabea8d92bf648e7a6275551273d48719efb0523
antoine zimmermann <antoine.zimmermann@storetail.com>
2017-10-22 13:48:41 +0200

    load chapters

--- src/App.js
+++ src/App.js
@@ -8,10 +8,40 @@ import {
 import { connect } from "react-redux";
 import { ACTIONS } from "data/store";
 
+const TableOfContentsItemView = ({ path, content, name, type }) => {
+    const link = content ?
+        <a>Read</a>:
+        <p>Not Loaded</p>
+
+    return (
+        <li>
+            <p>
+                <h3>{name}</h3>
+                {link}
+            </p>
+        </li>
+    );
+};
+const TableOfContentsView = ({ chapters = [] }) => {
+
+    const toc = chapters
+        .filter(chapter => chapter.type === "file")
+        .map(chapter => (
+            <TableOfContentsItemView 
+                {...chapter}
+                key={chapter.path}
+            />
+        ));
+
+    return (
+        <ul>
+            {toc} 
+        </ul>
+    );
+
+};
 class AppView extends Component {
     componentWillMount () {
-
-        this.props.load();
 
     }
     render() {
@@ -21,6 +51,9 @@ class AppView extends Component {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                 </header>
+                <TableOfContentsView 
+                    chapters={this.props.chapters}
+                />
             </div>
         );
     }
--- src/data/store.js
+++ src/data/store.js
@@ -1,15 +1,20 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { createMiddleware } from "xstream-redux-observable";
 import { getContents } from "./api";
-import xs from "xstream";
+import xs, { merge } from "xstream";
+import sampleCombine from "xstream/extra/sampleCombine";
+import delay from "xstream/extra/delay";
 
 export const ACTIONS = {
     loaded: "chapters-loaded",
-    load: "load-chapters"
+    load: "load-chapters",
+    contentLoaded: "chapter-content-loaded"
 };
 const defaultState = {
     chapters: ["yolo"]
 };
+
+
 function book ( state = defaultState, action ) {
     const { type, data } = action
     switch ( type ) {
@@ -19,6 +24,18 @@ function book ( state = defaultState, action ) {
                 ...state,
                 chapters: data
             };
+        case ACTIONS.contentLoaded:
+            return {
+                ...state,
+                chapters: state.chapters.map(( current ) => {
+                    if( current.path === data.path ) {
+                        return {
+                            ...current,
+                            ...data
+                        };
+                    } else return current;
+                })
+            };
 
         default:
             return state;
@@ -27,26 +44,73 @@ function book ( state = defaultState, action ) {
 
 }
 
+const author= "sjl";
+const repo= "learnvimscriptthehardway";
 const loadBook = () => {
 
-    return xs.fromPromise(getContents({
-        author: "sjl",
-        repo: "learnvimscriptthehardway",
+    const chapters$ = xs.fromPromise(getContents({
+        author,
+        repo,
         path: "chapters"
     }));
 
+    const data$ = chapters$
+        .map( chapters => {
+
+            const period = 1000;
+            const chaptersArr$ = chapters.map( (c, index) => {
+                return xs.of(c)
+                    .compose(delay(index * period))
+                    .map(c => xs.fromPromise(getContents({
+                        author,
+                        repo,
+                        path: c.path
+                    })))
+                    .flatten();
+            } );
+
+            return xs.merge.apply(
+                null,
+                chaptersArr$
+            )
+                .replaceError( e => {
+                    console.error(e);
+                    return xs.empty();
+                });
+        })
+        .flatten()
+        .debug("chapter");
+
+    return {
+        chapters$,
+        data$
+    };
 };
 
 const middleware = createMiddleware( action$ => {
 
-    const chapters$ = loadBook()
+    const { chapters$, data$ } = loadBook()
+
+    const loaded$ = chapters$
         .debug("chapters")
         .map( chapters => ({
             type: ACTIONS.loaded,
             data: chapters
         }) );
 
-    return chapters$;
+
+    const content$ = data$
+        .debug("content")
+        .map(chapter => ({
+            type: ACTIONS.contentLoaded,
+            data: chapter
+        }))
+        .replaceError(e => {
+            console.error(e);
+            return xs.empty();
+        });
+
+    return xs.merge(loaded$, content$);
 });
 
 export const store = createStore(
