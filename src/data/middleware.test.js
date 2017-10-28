40ccae9293e2c7651a52b64c0a58f47da95f2fe5
antoine zimmermann <antoine.zimmermann@webdia-group.com>
2017-10-28 19:02:13 +0200

    add tests for pouchdb api

--- src/data/middleware.test.js
+++ src/data/middleware.test.js
@@ -62,9 +62,9 @@ describe("put", () => {
     });
 
     const { ok, id, rev } = response;
-      expect(ok).toBe(true);
-      expect(id).toBe("lol");
-      expect(typeof rev).toBe("string");
+    expect(ok).toBe(true);
+    expect(id).toBe("lol");
+    expect(typeof rev).toBe("string");
 
     return await db.destroy();
   });
@@ -99,6 +99,119 @@ describe("put", () => {
 
     return await db.destroy();
 
+
+  });
+
+  test("update", async function () {
+
+    expect.hasAssertions();
+    const db = new PouchDB("db-test");
+    const data = {
+      foo: "bar"
+    };
+
+    const response = await db.put({
+      _id: "lol",
+      data
+    });
+
+
+    try {
+      const final = await db.put({
+        _id: response.id,
+        _rev: response.rev,
+        data: {
+          foo: "baz"
+        }
+      });
+
+      const { ok, rev, id } = final;
+      expect(ok).toBe(true);
+      expect(id).toBe("lol");
+      expect(typeof rev).toBe("string");
+
+    } catch ( e ) {
+      console.log(e);
+    }
+
+    return await db.destroy();
+  });
+
+  describe("bulk add", () => {
+
+    test("200", async function () {
+
+      expect.hasAssertions();
+
+      const messages = [ "bar", "baz", "boz" ].map(( m, i ) => ({
+        _id: i + "_doc",
+        data: {
+          foo: m
+        }
+      }));
+
+      const db = new PouchDB("db-test");
+
+      try {
+        const response = await db.bulkDocs(messages);
+
+        response.forEach(({ ok, id, rev }) => {
+          expect(ok).toBe(true);
+          expect(typeof id).toBe("string");
+          expect(typeof rev).toBe("string");
+        });
+
+      } catch ( e ) {
+        console.log(e);
+      }
+
+      return await db.destroy();
+
+    });
+
+    test("update - with errors", async function () {
+
+      expect.hasAssertions();
+
+      const messages = [ "bar", "baz" ].map(( m, i ) => ({
+        _id: i + "_doc",
+        data: {
+          foo: m
+        }
+      }));
+
+      const db = new PouchDB("db-test");
+
+      try {
+        const response = await db.bulkDocs(messages);
+
+        const updates = messages.map(( m, i ) => ({
+          _id: response[i].id,
+          _rev: i !== 1 ? "" : response[i].rev,
+          data: {
+            foo: "bar"
+          }
+        }));
+
+        const final = await db.bulkDocs(updates);
+        const [ r1, r2 ] = final;
+
+        expect(r1.status).toEqual(409);
+        expect(r1.error).toEqual(true);
+        expect(r1.id).toEqual(messages[0]._id);
+
+        expect(r2).toMatchObject({
+          ok: true,
+          id: messages[1]._id
+        });
+
+      } catch ( e ) {
+        console.log(e);
+      }
+
+      return await db.destroy();
+
+    });
 
   });
 });
